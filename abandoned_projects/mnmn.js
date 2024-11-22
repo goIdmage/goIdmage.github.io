14a53fbc3f86f61611dbefb9f55870a891eab167
TA <117796466+goIdmage@users.noreply.github.com>
2024-11-22 05:29:08 +0000

    Add files via upload

---
+++ abandoned_projects/mnmn.js
@@ -0,0 +1,14 @@
+function right(e) {
+if (navigator.appName == 'Netscape' && 
+(e.which == 3 || e.which == 2))
+return false;
+else if (navigator.appName == 'Microsoft Internet Explorer' && 
+(event.button == 2 || event.button == 3)) {
+alert("ii dont thiink you 2hould do that.");
+return false;
+}
+return true;
+}
+document.onmousedown=right;
+if (document.layers) window.captureEvents(Event.MOUSEDOWN);
+window.onmousedown=right;
